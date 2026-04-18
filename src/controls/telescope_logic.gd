2c6b6894b175286693d5ba2a4c56580d11377f8f
Samokhvalov Zahar <1samokhvalov.zs@gmail.com>
2026-04-19 00:15:54 +0300

    Add loop logic

--- src/controls/telescope_logic.gd
+++ src/controls/telescope_logic.gd
@@ -1,5 +1,15 @@
 extends Button
 
+@export_node_path("ColorRect") var loupe_overlay_path: NodePath = NodePath("../../../../LoupeOverlay")
+
+@onready var loupe_overlay: ColorRect = get_node_or_null(loupe_overlay_path) as ColorRect
+
+
+func _ready() -> void:
+	toggle_mode = true
+	text = "Loupe"
+
 
 func _on_button_up() -> void:
-	pass # Replace with function body.
+	if loupe_overlay != null and loupe_overlay.has_method("set_loupe_enabled"):
+		loupe_overlay.call("set_loupe_enabled", button_pressed)
